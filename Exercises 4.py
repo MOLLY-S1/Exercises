ab9204a5a60ae324431f9d2503e12bbb22492732
MOLLY-S1 <sankeym@middleton.school.nz>
2022-02-24 12:01:45 +1300

    exercises

---
+++ Exercises 4.py
@@ -0,0 +1,13 @@
+numbers = [10, 20, 30, 40]
+fruit = ["pineapple", "apple", "banana"]
+print(numbers[0:2])
+print(fruit[1:3])
+print('apple'in fruit)
+fruit.append('orange')
+print(fruit)
+fruit.insert(3, 'grapes')
+print(fruit)
+del fruit[1]
+print(fruit)
+for num in list:
+    print(num **2)
